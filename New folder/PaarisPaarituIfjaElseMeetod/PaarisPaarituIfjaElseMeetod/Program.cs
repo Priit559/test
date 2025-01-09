03f74bad6ea0394fbd364378996187116002de61
Priit559 <priitkask559@gmail.com>
2025-01-09 17:08:07 +0200

    PaarisPaaritudIfjaElseMeetod

---
+++ New folder/PaarisPaarituIfjaElseMeetod/PaarisPaarituIfjaElseMeetod/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PaarisPaarituIfjaElseMeetod
+{
+    internal class Program
+    {
+        public static bool OnPaaris(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+              
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Sisesta number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            { 
+                if (OnPaaris(number))
+                {
+                    Console.WriteLine($"{number} on paarisarv.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} on paaritu arv.");
+                }
+            
+
+            }
+            else
+            {
+                Console.WriteLine("Vigane sisend. Palun sisesta täisarv.");
+            }
+            Console.ReadKey();
+
+        }
+
+
+        
+    }
+}
