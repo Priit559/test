dc60be7619d1b37947b551ec410e2ff5d91d39ae
Priit559 <priitkask559@gmail.com>
2025-01-09 13:36:47 +0200

    init

---
+++ IfandElse/IfandElse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfandElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sisesta oma vanus: ");
+            
+            int vanus = int.Parse(Console.ReadLine());
+            
+            if (vanus < 18 )
+            
+            {
+                Console.WriteLine("Oled alaealine.");
+            }
+            else if (vanus >= 19 && vanus < 65)
+            {
+                Console.WriteLine("Oled täisealine.");
+            }
+            else
+            {
+                Console.WriteLine("Oled pensionär.");
+            }
+                
+                
+
+
+
+        }
+    }
+}
