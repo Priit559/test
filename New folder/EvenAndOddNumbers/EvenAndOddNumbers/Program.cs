56a91398768b5aaad569c863c6276be06a1c5a4b
Priit559 <priitkask559@gmail.com>
2025-01-09 14:39:23 +0200

    EvenOddNumbers

---
+++ New folder/EvenAndOddNumbers/EvenAndOddNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EvenAndOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sisesta number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number} on paarisarv.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} on paaritu arv.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vigane sisend. Palun sisesta täisarv.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
