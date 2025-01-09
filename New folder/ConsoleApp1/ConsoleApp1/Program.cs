79105964a81b5f93c13733dba3eaaf7c0cb15ee9
Priit559 <priitkask559@gmail.com>
2025-01-09 14:10:33 +0200

    nesting

---
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oled sa poiss või tüdruk? (p/t): ");
+            char sugu = char.Parse(Console.ReadLine());
+
+            if (sugu == 'p')
+            {
+                Console.WriteLine("Sa oled noormees.");
+
+            }
+            else if (sugu == 't')
+            {
+                Console.WriteLine("Sa oled neiu.");
+            }
+            else
+            {
+                Console.WriteLine("Viga! Sa sisestasid vale soo.");
+            }
+
+        }
+    }
+}
