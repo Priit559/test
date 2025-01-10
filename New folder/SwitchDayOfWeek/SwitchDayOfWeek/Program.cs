0cb749d8fb7df4e274a4fa071e8ce1f3785d31aa
Priit559 <priitkask559@gmail.com>
2025-01-10 10:35:04 +0200

    DayOfTheWeekSwitch

---
+++ New folder/SwitchDayOfWeek/SwitchDayOfWeek/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SwitchDayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            DayOfWeek dayOfWeek = today.DayOfWeek;
+
+            switch (dayOfWeek)
+
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Täna on Esmaspäev");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Täna on Teisipäev");
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Täna on Kolmapäev");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Täna on Neljapäev");
+                    break;
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Täna on Reede");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Täna on Laupäev");
+                    break;
+
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Täna on Pühapäev");
+                    break;
+
+                default:
+                    Console.WriteLine("Viga!");
+                    break;
+            }
+
+
+
+
+
+
+        }
+    }
+}
