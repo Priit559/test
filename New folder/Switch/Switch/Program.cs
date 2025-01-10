6997b0c7495d4b6b4de918d430682e4847067b1b
Priit559 <priitkask559@gmail.com>
2025-01-10 10:19:02 +0200

    SwitchRandomNumber

---
+++ New folder/Switch/Switch/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int randomNumber = random.Next(1, 6);
+
+            switch (randomNumber)
+            {
+                case 1:
+                    Console.WriteLine("Üks");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Kaks");
+                    break;
+
+                    case 3:
+                    Console.WriteLine("Kolm");
+                    break;
+
+                    case 4:
+                    Console.WriteLine("Neli");
+                    break;
+
+                    case 5:
+                    Console.WriteLine("Viis");
+
+                    break;
+                    case 6:
+                    Console.WriteLine("Kuus");
+                    break;
+
+                    default:
+                    Console.WriteLine("Viga: number peab olema vahemikus 1 kuni 6.");
+                    break;
+
+
+
+
+            }
+
+        }
+    }
+}
