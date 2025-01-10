cb2e11e3fcdbeafecc87e76cbc7af446ef8e356a
Priit559 <priitkask559@gmail.com>
2025-01-10 11:31:54 +0200

    SwitchCoffePrice

---
+++ New folder/SwitchCoffePrice/SwitchCoffePrice/Program.cs
@@ -0,0 +1,49 @@
+﻿
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Fantaasia kohvik!");
+        Console.WriteLine("\n\nMenüü (kohvi valik)\n\n");
+        Console.WriteLine("1. Vaike tass");
+        Console.WriteLine("2. Keskmine tass");
+        Console.WriteLine("3. Suur tass");
+        Console.WriteLine("\n\nPalun tee valik numbriga: ");
+
+        if (byte.TryParse(Console.ReadLine(), out byte coffeeSize))
+        {
+            double price = 0;
+
+            switch (coffeeSize)
+            {
+                case 1:
+                    price = 1.5;
+                    break;
+
+                case 2:
+                    price = 2.5;
+                    break;
+
+                case 3:
+                    price = 3;
+                    break;
+
+                default:
+                    Console.WriteLine("Selline valik puudub.");
+                    break;
+            }
+
+            Console.WriteLine($"Teie valiku hind on: {price} eurot.");
+        }
+        else
+        {
+
+            Console.WriteLine("Vigane sisestus. Palun sisesta number.");
+        }
+
+
+        Console.ReadKey();
+
+    }
+}
+
